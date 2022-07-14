8b690fbe19a11682b060e9a46ac23cb03395e2f2
Evgen <Francesbean1999166@mail.ru>
2022-07-14 10:50:42 +0300

    нахождение четности числа с выведением всех четных находящихся от 1 до N(a)

---
+++ ex8/Program.cs
@@ -0,0 +1,28 @@
+﻿int a;
+int b;                          
+Console.Write("введите число: ");
+a = Console.Read();
+b = a - 48;
+if (a % 2==1)
+{
+    Console.WriteLine("число нечетное");
+    b = b + 1;
+    for (int i = 0; i < b; i++)
+    {
+       if(i % 2==0) Console.WriteLine(i);
+    }
+}
+ else
+{
+    Console.WriteLine("Число четное");
+    for (int i = 0; i < b + 1 ; i++)
+    {
+       if(i % 2==0) Console.WriteLine(i);
+    }
+} 
+
+
+
+
+
+

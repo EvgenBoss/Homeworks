3fed5986b224aeaa08470f0fa6a9d17ebc48e6a3
Evgen <Francesbean1999166@mail.ru>
2022-07-20 08:10:14 +0300

    Полиндром

---
+++ ex19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+string s = a.ToString();
+string result = s;
+char[] ar = s.ToCharArray();
+Array.Reverse(ar);
+s = new String(ar);
+a = Convert.ToInt32(s);
+if(result == s)
+{
+    Console.WriteLine("Число является полиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является полиндромом");
+}

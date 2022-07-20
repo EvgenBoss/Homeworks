8a4d5d114f074fb9d91b49b6521e40b8e21bf894
Evgen <Francesbean1999166@mail.ru>
2022-07-20 07:48:36 +0300

    Расстояние меж точками

---
+++ ex21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите x1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите x2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Расстояние меж точками = ");
+Console.WriteLine(Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1)));

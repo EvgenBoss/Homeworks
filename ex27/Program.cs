417893a6a911741bf923acc693fd65f0c5b172d6
Evgen <Francesbean1999166@mail.ru>
2022-07-23 23:56:48 +0300

    сумма цифр

---
+++ ex27/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+for(int i = 10; x > i/10; i = i * 10)
+{
+        summ = summ + (x * 10) / i % 10;
+}
+Console.WriteLine(summ);

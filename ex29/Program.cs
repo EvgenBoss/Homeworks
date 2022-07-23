c0a65cfa24241cc3ecd8e72b7c45b8fd59c76679
Evgen <Francesbean1999166@mail.ru>
2022-07-23 23:31:06 +0300

    вывод вочьмизначного массива

---
+++ ex29/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] Array = new int[8];
+{
+    for(int i = 0; i < Array.Length; i++)
+
+        Array[i] = new Random().Next(1,10);
+}
+var str = string.Join(" ", Array);
+Console.WriteLine(str);

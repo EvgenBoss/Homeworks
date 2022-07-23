68f24aa624c0bf1da9f29fdc0fdf6c1e1368d3fd
Evgen <Francesbean1999166@mail.ru>
2022-07-23 23:22:03 +0300

    возведение в степень с проверкой

--- ex25/Program.cs
+++ ex25/Program.cs
@@ -1,10 +1,17 @@
 ﻿void Funk(int a,int b)
 {
     int mtp = a;
+    if (a==0)
+    { Console.WriteLine("0");}
+    else if (a==1)
+    { Console.WriteLine("1");}
+    else
+    {
     for(int i = 0; i < b-1; i++ )
     {
         mtp = mtp * a;
     }
     Console.WriteLine(mtp);
 }
-Funk (5,10);
+}
+Funk (3,4);
